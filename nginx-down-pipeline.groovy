4afeb0b15bb60a5b53c6d07e8429b9e759a703b2
Nikita Zim <n_zim_r@mail.ru>
2025-01-17 18:34:05 +0500

    new nginx-down-pipeline file

---
+++ nginx-down-pipeline.groovy
@@ -0,0 +1,34 @@
+pipeline {
+    agent any
+    environment {
+        KUBECONFIG = '/home/thereiv/.kube/config'
+    }
+
+    stages {
+        stage('Get pods and services'){
+            steps{
+                script{
+                    sh 'kubectl get pods'
+                    sh 'kubectl get service'
+                }
+            }
+        }
+        stage('Delete nginx server'){
+            steps{
+                script{
+                    sh 'kubectl delete -f nginx-pod.yaml'
+                    sh 'kubectl delete -f nginx-service.yaml'
+                    sh 'sleep 2'
+                }
+            }
+        }
+        stage('Get pods and services'){
+            steps{
+                script{
+                    sh 'kubectl get pods'
+                    sh 'kubectl get service'
+                }
+            }
+        }
+    }
+}
